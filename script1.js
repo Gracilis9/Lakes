bf876a776633275e13339e21a60ab1a6e2bc8688
Gracilis9 <71337740+Gracilis9@users.noreply.github.com>
2020-09-15 12:08:33 -0400

    It's a summary
    Yes.

---
+++ script1.js
@@ -0,0 +1,6 @@
+window.alert("it's me!");
+let name = window.prompt("Who are you?");
+window.alert("Welcome," + name);
+
+let middleDiv = document.getElementById("greeting");
+greetingDiv.innerHTML = "<p>Welcome, " + name + ""</p>";
